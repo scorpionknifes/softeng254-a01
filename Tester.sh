59fa926ed806871664d755927dae45f87caaf244
ZHENK <chengzhenyang@gmail.com>
2020-08-11 17:43:28 +1200

    first commit

---
+++ Tester.sh
@@ -0,0 +1,7 @@
+for jar in `ls [bg]*.jar `
+do
+    printf "_________ $jar _________\n\n"
+    javac -d "./build" -cp "./hamcrest-core-1.3.jar;./junit-4.13.jar;./$jar" "./TestFlushLeft.java"
+    java -cp "./hamcrest-core-1.3.jar;./junit-4.13.jar;./build;./$jar" org.junit.runner.JUnitCore text.TestFlushLeft
+    printf "\n"
+done
