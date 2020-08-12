f4c0af4671c0050594b203122e68381d7848590b
ZHENK <chengzhenyang@gmail.com>
2020-08-12 16:22:17 +1200

    TestFlushLeft

--- src/text/TestFlushLeft.java
+++ src/text/TestFlushLeft.java
@@ -51,12 +51,104 @@ public class TestFlushLeft {// DO NOT CHANGE THE CLASS NAME OR YOU WILL GET ZERO
 
     @Test
     public void TestEmptyInput() {
-        assertEquals("[]", Formatter.flushLeftText("", 10).toString());
+        List<String> expected = new ArrayList<String>();
+        expected.add("");
+        assertEquals(expected, Formatter.flushLeftText("", 10));
     }
 
     @Test
-    public void TestGeneralInput() {
-        String result = Formatter.flushLeftText("12345678901234567890", 10).toString();
-        System.out.println(result);
+    public void TestShortInput() {
+        List<String> expected = new ArrayList<String>();
+        expected.add("sample");
+        assertEquals(expected, Formatter.flushLeftText("sample", 10));
+    }
+
+    @Test
+    public void TestExtraLongInput() {
+        List<String> actual = Formatter.flushLeftText("12345678901234567890", 10);
+        List<String> expected = new ArrayList<String>();
+        expected.add("123456789-");
+        expected.add("012345678-");
+        expected.add("90");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void TestLongInput(){
+        List<String> actual = Formatter.flushLeftText("1234567890123456789", 10);
+        System.out.println(actual);
+        List<String> expected = new ArrayList<String>();
+        expected.add("123456789-");
+        expected.add("0123456789");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestOneSpacesInput(){
+        List<String> actual = Formatter.flushLeftText("1 2 3 4 5 6 7 8 9 0", 10);
+        System.out.println(actual);
+        List<String> expected = new ArrayList<String>();
+        expected.add("1 2 3 4 5");
+        expected.add("6 7 8 9 0");
+        assertEquals(expected, actual);
+    }
+
+    /* 
+        TestSpaceFrontInput
+
+        Fails:
+        - badA.jar check for string.trim()
+    */
+    @Test
+    public void TestSpacesFrontInput(){
+        List<String> actual = Formatter.flushLeftText("   1234567890", 10);
+        System.out.println(actual);
+        List<String> expected = new ArrayList<String>();
+        expected.add("1234567890");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestSpacesBehindInput(){
+        List<String> actual = Formatter.flushLeftText("1234567890    ", 10);
+        System.out.println(actual);
+        List<String> expected = new ArrayList<String>();
+        expected.add("1234567890");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestSpacesMiddleInput(){
+        List<String> actual = Formatter.flushLeftText("12345   67890", 10);
+        System.out.println(actual);
+        List<String> expected = new ArrayList<String>();
+        expected.add("12345");
+        expected.add("67890");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestNewLinesInput(){
+        List<String> actual = Formatter.flushLeftText("\n1\n2\n3\n4\n5\n6\n", 10);
+        System.out.println(actual);
+        List<String> expected = new ArrayList<String>();
+        expected.add("1");
+        expected.add("2");
+        expected.add("3");
+        expected.add("4");
+        expected.add("5");
+        expected.add("6");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestDoubleNewLinesInput(){
+        List<String> actual = Formatter.flushLeftText("1\n\n2\n\n3", 10);
+        System.out.println(actual);
+        List<String> expected = new ArrayList<String>();
+        expected.add("1");
+        expected.add("2");
+        expected.add("3");
+        assertEquals(expected, actual);
     }
 }
